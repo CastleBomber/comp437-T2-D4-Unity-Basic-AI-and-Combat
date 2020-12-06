775a619044a55421acb6964e1b89348c82285b7c
CastleBomber <richie.ucsb@gmail.com>
2020-12-05 21:15:03 -0800

    addedFlickerEnemyHit

--- Unity Basic AI and Combat/Assets/Scripts/MonoBehaviour/Character.cs
+++ Unity Basic AI and Combat/Assets/Scripts/MonoBehaviour/Character.cs
@@ -15,4 +15,12 @@ public abstract class Character : MonoBehaviour
     public abstract void ResetCharacter(); // must be implemented by child class
 
     public abstract IEnumerator DamageCharacter(int damage, float interval); // IE means coroutine
+
+    public virtual IEnumerator FlickerCharacter()
+	{
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+	}
 }
--- Unity Basic AI and Combat/Assets/Weapon.cs
+++ Unity Basic AI and Combat/Assets/Weapon.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+
 public class Weapon : MonoBehaviour
 {
     public GameObject ammoPrefab;
     static List<GameObject> ammoPool;
     public int poolSize;
 	public float weaponVelocity;
+	bool isFiring;
+	[HideInInspector]
+	public Animator animator;
+	Camera localCamera;
+	float positiveSlope;
+	float negativeSlope;
+
+	enum Quadrant
+	{
+		East,
+		South,
+		West,
+		North
+	}
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+		isFiring = false;
+		localCamera = Camera.main;
+
+		Vector2 lowerLeft = localCamera.ScreenToWorldPoint(new Vector2(0, 0));
+		Vector2 upperRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+		Vector2 upperLeft = localCamera.ScreenToWorldPoint(new Vector2(0, Screen.height));
+		Vector2 lowerRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, 0));
+
+		positiveSlope = GetSlope(lowerLeft, upperRight);
+		negativeSlope = GetSlope(upperLeft, lowerRight);
+	}
 
 	private void Awake()
 	{
@@ -29,8 +60,11 @@ public class Weapon : MonoBehaviour
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
+			isFiring = true;
 			FireAmmo();
 		}
+
+		UpdateState();
 	}
 
 	public GameObject SpawnAmmo(Vector3 location)
@@ -64,5 +98,94 @@ public class Weapon : MonoBehaviour
 	private void OnDestroy()
 	{
 		ammoPool = null;
+	}
+
+	float GetSlope(Vector2 pointOne, Vector2 pointTwo)
+	{
+		return (pointTwo.y - pointOne.y) / (pointTwo.x - pointOne.x);
+	}
+
+	bool HigherThanPositiveSlopeLine(Vector2 inputPosition)
+	{
+		Vector2 playerPosition = gameObject.transform.position;
+		Vector2 mousePosition = localCamera.ScreenToWorldPoint(inputPosition);
+		float yIntercept = playerPosition.y - (positiveSlope * playerPosition.x);
+		float inputIntercept = mousePosition.y - (positiveSlope * mousePosition.x);
+
+		return inputIntercept > yIntercept;
+	}
+
+	bool HigherThanNegativeSlopeLine(Vector2 inputPosition)
+	{
+		Vector2 playerPosition = gameObject.transform.position;
+		Vector2 mousePosition = localCamera.ScreenToWorldPoint(inputPosition);
+
+		float yIntercept = playerPosition.y - (negativeSlope * playerPosition.x);
+		float inputIntercept = mousePosition.y - (negativeSlope * mousePosition.x);
+
+		return inputIntercept > yIntercept;
+	}
+
+	Quadrant GetQuadrant()
+	{
+		Vector2 mousePosition = Input.mousePosition;
+		Vector2 playerPosition = transform.position;
+
+		bool higherThanPositiveSlopeLine = HigherThanPositiveSlopeLine(Input.mousePosition);
+		bool higherThanNegativeSlopeLine = HigherThanNegativeSlopeLine(Input.mousePosition);
+
+		if (!higherThanPositiveSlopeLine && higherThanNegativeSlopeLine)
+		{
+			return Quadrant.East;
+		}
+		else if(!higherThanPositiveSlopeLine && !higherThanNegativeSlopeLine)
+		{
+			return Quadrant.South;
+		}
+		else if(higherThanPositiveSlopeLine && !higherThanNegativeSlopeLine)
+		{
+			return Quadrant.West;
+		}
+		else
+		{
+			return Quadrant.North;
+		}
+	}
+
+	void UpdateState()
+	{
+		if (isFiring)
+		{
+			Vector2 quadrantVector;
+			Quadrant quadEnum = GetQuadrant();
+
+			switch (quadEnum)
+			{
+				case Quadrant.East:
+					quadrantVector = new Vector2(1.0f, 0.0f);
+					break;
+				case Quadrant.South:
+					quadrantVector = new Vector2(0.0f, -1.0f);
+					break;
+				case Quadrant.West:
+					quadrantVector = new Vector2(-1.0f, 1.0f);
+					break;
+				case Quadrant.North:
+					quadrantVector = new Vector2(0.0f, 1.0f);
+					break;
+				default:
+					quadrantVector = new Vector2(0.0f, 0.0f);
+					break;
+			}
+			animator.SetBool("isFiring", true);
+			animator.SetFloat("fireXDir", quadrantVector.x);
+			animator.SetFloat("fireYDir", quadrantVector.y);
+
+			isFiring = false;
+		}
+		else
+		{
+			animator.SetBool("isFiring", false);
+		}
 	}
 }
